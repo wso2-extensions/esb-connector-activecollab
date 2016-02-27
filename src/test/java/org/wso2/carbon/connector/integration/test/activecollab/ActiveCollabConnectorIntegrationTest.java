d2c1ba1844cbbfb275c24088d280e1163b1fef5a
Kesavan <ykesan@gmail.com>
2016-02-27 20:33:43 +0530

    version changed.

--- src/test/java/org/wso2/carbon/connector/integration/test/activecollab/ActiveCollabConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/activecollab/ActiveCollabConnectorIntegrationTest.java
@@ -46,7 +46,7 @@ public class ActiveCollabConnectorIntegrationTest extends ConnectorIntegrationTe
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("activecollab-connector-1.0.0");
+        init("activecollab-connector-1.0.1-SNAPSHOT");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
